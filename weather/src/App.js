1f364f612ce00dd6ab36c0b6027a32bfd926da00
Mar4ik_ <brainocswell@gmail.com>
2022-08-29 14:57:28 +0300

    background gonfig weather

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,27 @@
-
 import './App.css';
+import ShowWeather from "./components/ShowWeather";
+import {useState} from "react";
+
 
 function App() {
+
+  const [city,setCity]= useState()
+  const [value,setValue]= useState()
+
+  const handler = (event) => {
+    setValue(event.currentTarget.value)
+  }
+
   return (
     <div className="App">
+      <div className='background'>
+        <div className='main'>
+          <input type="text" onChange={event => handler(event)} />
+          <button onClick={() => {setCity(value)}}>Search</button>
+        </div>
+      </div>
 
+      {city?<ShowWeather city={city}/> : null}
     </div>
   );
 }
